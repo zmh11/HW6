53b9eda53494f262be9bde07eaf92567cfee5133
Zachary Hickey <zmhickey@Zacharys-MBP.local>
2020-10-24 15:47:19 -0500

    + - *

--- calc.cpp
+++ calc.cpp
@@ -18,24 +18,47 @@ bool IsNumber(const std::string& s){
 void calc (){
   Dlist<double> calc;
   std::string input = "";
-  
+  int size = 0;
+  double o1; // operand 1
+  double o2; // operand 2
   while( input.compare("q") ){
     std::cin >> input;
     
-    if( IsNumber(input) )
+    if( IsNumber(input) ){
       calc.InsertFront( std::stod(input) );
-    else if( input.compare("+") ){
-      
+      size++;
+    } else if( (input == "+" )&& (size >= 2) ){ // add
+      o1 = calc.RemoveFront();
+      o2 = calc.RemoveFront();
+      calc.InsertFront( (o1 + o2) );
+      size --;
+    } else if( (input == "-") && (size >= 2) ){ // subtract
+      o1 = calc.RemoveFront();
+      o2 = calc.RemoveFront();
+      calc.InsertFront( (o1 - o2) );
+      size --;
+    }else if ( (input == "*") && (size>=2) ){
+      o1 = calc.RemoveFront();
+      o2 = calc.RemoveFront();
+      calc.InsertFront( (o1 * o2) );
+      size --;
     }
-      
+  
+  std::cout<<std::endl;
+  }
 
 
 
+    while (!calc.IsEmpty())
+  {
+    o1 = calc.RemoveFront();
+    std::cout<< o1 <<" ";
   }
 
 }
 
 int main(){
-  std::cout<< " "<< IsNumber("12") <<IsNumber("1.5");
+  //std::cout<< " "<< IsNumber("12") <<IsNumber("1.5");
   calc();
+  
 }
